0b81a838b568c0f3593c373b3071d33fa979b5e7
PawelPodkalicki <ppodkali@gmail.com>
2019-03-20 11:18:05 +0100

    Add adpanel brand assets, fix unbound variable

--- src/configure.sh
+++ src/configure.sh
@@ -100,7 +100,8 @@ then
     ADPANEL_ENDPOINT="https://example.com"
     read_option ADPANEL_ENDPOINT "External adselect service endpoint" 1
 else
-    read_option ADPANEL_BRAND_ASSETS_DIR "Directory where custom brand assets are stored. If do not exist, standard will be used" 1
+    ADPANEL_BRAND_ASSETS_DIR=${ADPANEL_BRAND_ASSETS_DIR:-""}
+    read_option ADPANEL_BRAND_ASSETS_DIR "Directory where custom brand assets are stored. If dir does not exist, standard assets will be used" 1
     if [ ! -d "${ADPANEL_BRAND_ASSETS_DIR}" ]
     then
         echo "Directory ${ADPANEL_BRAND_ASSETS_DIR} doesn't exist."
