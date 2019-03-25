dc1249ab7bb447c836e47cc802b33cf50dab7b4a
Paweł Zaremba <pawzar@gmail.com>
2019-03-25 18:25:52 +0100

    www-data

--- src/prepare-directories.sh
+++ src/prepare-directories.sh
@@ -6,7 +6,7 @@ id --user ${VENDOR_USER} &>/dev/null || useradd --create-home --shell /bin/bash 
 
 mkdir -p ${VENDOR_DIR}
 
-chown -R ${VENDOR_USER}:`id --group --name ${VENDOR_USER}` ${VENDOR_DIR}
+chown -R ${VENDOR_USER}:www-data ${VENDOR_DIR}
 
 mkdir -p ${BACKUP_DIR}
 mkdir -p ${DATA_DIR}
@@ -14,7 +14,7 @@ mkdir -p ${LOG_DIR}
 mkdir -p ${RUN_DIR}
 mkdir -p ${SCRIPT_DIR}
 
-chown -R ${VENDOR_USER}:`id --group --name ${VENDOR_USER}` ${BACKUP_DIR} ${DATA_DIR} ${LOG_DIR} ${RUN_DIR} ${SCRIPT_DIR}
+chown -R ${VENDOR_USER}:www-date ${BACKUP_DIR} ${DATA_DIR} ${LOG_DIR} ${RUN_DIR} ${SCRIPT_DIR}
 
 mkdir -p ${ETC_DIR}
 mkdir -p ${CONFIG_DIR}
