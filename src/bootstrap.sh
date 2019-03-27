dd9b738b195a6d476f80d80c889909137d1f1da5
Paweł Zaremba <pawzar@gmail.com>
2019-03-27 17:20:45 +0100

    mongo

--- src/bootstrap.sh
+++ src/bootstrap.sh
@@ -13,11 +13,6 @@ echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.lis
 
 # ===
 
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
-echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
-
-# ===
-
 add-apt-repository --yes ppa:adshares/releases
 add-apt-repository --yes ppa:certbot/certbot
 
@@ -82,7 +77,7 @@ apt-get --yes update
 apt-get --yes --no-install-recommends install \
     python python-pip python-dev gcc \
     php7.2-fpm php7.2-mysql php7.2-bcmath php7.2-bz2 php7.2-curl php7.2-gd php7.2-intl php7.2-mbstring php7.2-sqlite3 php7.2-zip php7.2-simplexml php-apcu \
-    ads nginx percona-server-server-5.7 percona-server-client-5.7 nodejs yarn mongodb-org \
+    ads nginx percona-server-server-5.7 percona-server-client-5.7 nodejs yarn mongodb \
     certbot python-certbot-nginx
 
 # ===
