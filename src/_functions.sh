2f411e8c7c298c4dfc1eff23a1b59cfb370aee68
Paweł Zaremba <pawzar@gmail.com>
2019-04-10 14:55:46 +0200

    Colors

--- src/_functions.sh
+++ src/_functions.sh
@@ -4,6 +4,79 @@ test ${_FUNCTIONS_FILE_WAS_LOADED:-0} -eq 1 && echo "Functions file was already 
 _FUNCTIONS_FILE_WAS_LOADED=1
 _CONFIG_VARS=()
 
+# Reset
+ColorOff='\e[0m'        # Text Reset
+
+# Regular Colors
+Black='\e[0;30m'        # Black
+Red='\e[0;31m'          # Red
+Green='\e[0;32m'        # Green
+Yellow='\e[0;33m'       # Yellow
+Blue='\e[0;34m'         # Blue
+Purple='\e[0;35m'       # Purple
+Cyan='\e[0;36m'         # Cyan
+White='\e[0;37m'        # White
+
+# Bold
+BBlack='\e[1;30m'       # Black
+BRed='\e[1;31m'         # Red
+BGreen='\e[1;32m'       # Green
+BYellow='\e[1;33m'      # Yellow
+BBlue='\e[1;34m'        # Blue
+BPurple='\e[1;35m'      # Purple
+BCyan='\e[1;36m'        # Cyan
+BWhite='\e[1;37m'       # White
+
+# Underline
+UBlack='\e[4;30m'       # Black
+URed='\e[4;31m'         # Red
+UGreen='\e[4;32m'       # Green
+UYellow='\e[4;33m'      # Yellow
+UBlue='\e[4;34m'        # Blue
+UPurple='\e[4;35m'      # Purple
+UCyan='\e[4;36m'        # Cyan
+UWhite='\e[4;37m'       # White
+
+# Background
+On_Black='\e[40m'       # Black
+On_Red='\e[41m'         # Red
+On_Green='\e[42m'       # Green
+On_Yellow='\e[43m'      # Yellow
+On_Blue='\e[44m'        # Blue
+On_Purple='\e[45m'      # Purple
+On_Cyan='\e[46m'        # Cyan
+On_White='\e[47m'       # White
+
+# High Intensity
+IBlack='\e[0;90m'       # Black
+IRed='\e[0;91m'         # Red
+IGreen='\e[0;92m'       # Green
+IYellow='\e[0;93m'      # Yellow
+IBlue='\e[0;94m'        # Blue
+IPurple='\e[0;95m'      # Purple
+ICyan='\e[0;96m'        # Cyan
+IWhite='\e[0;97m'       # White
+
+# Bold High Intensity
+BIBlack='\e[1;90m'      # Black
+BIRed='\e[1;91m'        # Red
+BIGreen='\e[1;92m'      # Green
+BIYellow='\e[1;93m'     # Yellow
+BIBlue='\e[1;94m'       # Blue
+BIPurple='\e[1;95m'     # Purple
+BICyan='\e[1;96m'       # Cyan
+BIWhite='\e[1;97m'      # White
+
+# High Intensity backgrounds
+On_IBlack='\e[0;100m'   # Black
+On_IRed='\e[0;101m'     # Red
+On_IGreen='\e[0;102m'   # Green
+On_IYellow='\e[0;103m'  # Yellow
+On_IBlue='\e[0;104m'    # Blue
+On_IPurple='\e[0;105m'  # Purple
+On_ICyan='\e[0;106m'    # Cyan
+On_IWhite='\e[0;107m'   # White
+
 function versionFromGit {
     local _PWD
     if [[ -z ${1:-""} ]]
@@ -48,6 +121,11 @@ function showVars {
     fi
 }
 
+function colorize {
+    local _NAME=${1:-"ColorOff"}
+    echo -e "${!_NAME}"
+}
+
 # Usage: readOption <var_name> [<message> [<max_length> [<namespace]]]
 function readOption {
     local MESSAGE=${2:-""}
@@ -61,9 +139,8 @@ function readOption {
         VARNAME="${PREFIX}_${1}"
     fi
 
-    echo ""
+    local ORIGINAL="${!VARNAME:-""}"
 
-    local ORIGINAL=${!VARNAME:-""}
     if [[ ${MAX_LENGTH} -eq 1 ]]
     then
         if [[ "${ORIGINAL}" == "1" ]] || [[ "${ORIGINAL^^}" == "Y" ]]
@@ -74,20 +151,21 @@ function readOption {
         fi
 
         local REPLY
-        local _EXPR
-        read -e -p "${MESSAGE} (0 = no, 1 = yes) [${ORIGINAL}]: " -n ${MAX_LENGTH} REPLY
-
-        if [[ -z $REPLY ]]
+        read -e -p "`colorize BIWhite`${MESSAGE}`colorize` (`colorize BIRed`0 = no`colorize`, `colorize BIGreen`1 = yes`colorize`) [`colorize BIWhite`${ORIGINAL}`colorize`]: " -n ${MAX_LENGTH} REPLY
+        if [[ "${REPLY}" == "1" ]] || [[ "${REPLY^^}" == "Y" ]]
         then
+            REPLY=1
+        else
             REPLY=${ORIGINAL}
         fi
 
-        local _EXPR=`echo "${VARNAME}=\"\$REPLY\""`
-        echo "<<<E ${_EXPR}"
+        local _EXPR="${VARNAME}=${REPLY}"
         eval "${_EXPR}"
+#        echo "<<< ${_EXPR}"
     else
-        read -e -p "${MESSAGE}: " -i "${ORIGINAL}" -n ${MAX_LENGTH} ${VARNAME}
-        echo "<<<R ${VARNAME}=\"${!VARNAME}\""
+        read -e -p "`colorize BIWhite`${MESSAGE}`colorize`: `colorize BIGreen`" -i "${ORIGINAL}" -n ${MAX_LENGTH} ${VARNAME}
+        echo -n `colorize`
+#        echo "<<< ${VARNAME}=\"${!VARNAME}\""
     fi
 }
 
@@ -114,8 +192,16 @@ function configDefault {
         VALUE="$ORIGINAL"
     fi
 
-    local _EXPR=`echo "${VARNAME}=\"\${VALUE}\""`
-    echo ">>> ${_EXPR}"
+    local _EXPR
+
+    if [[ "${VALUE}" =~ ^[+-]?[0-9]+([.][0-9]+)?$ ]]
+    then
+        _EXPR="${VARNAME}=${VALUE}"
+    else
+        _EXPR="${VARNAME}=\"${VALUE}\""
+    fi
+
+#    echo ">>> ${_EXPR}"
     eval "${_EXPR}"
 
     _CONFIG_VARS+=(${VARNAME})
@@ -126,15 +212,27 @@ function configDefault {
 function configVars {
     local PREFIX=${1:-""}
 
-    for VARNAME in "${_CONFIG_VARS[@]}"
+    local KEY
+    local VAL
+
+    for VARNAME in ${_CONFIG_VARS[*]}
     do
         if [[ -z ${PREFIX} ]]
         then
-            echo "${VARNAME}=\"${!VARNAME}\""
+            KEY="${VARNAME}"
         elif [[ ${VARNAME} == ${PREFIX}_* ]]
         then
-            echo "${VARNAME:${#PREFIX}}=\"${!VARNAME}\""
+            KEY="${VARNAME:${#PREFIX}}"
         fi
+
+        if [[ "${!VARNAME}" =~ ^[+-]?[0-9]+([.][0-9]+)?$ ]]
+        then
+            VAL="${!VARNAME}"
+        else
+            VAL="\"${!VARNAME}\""
+        fi
+
+        echo "${KEY}=${VAL}"
     done
 }
 
@@ -147,7 +245,7 @@ save_env () {
     echo -n " < Preparing environment file: $2"
 
     local _INITIAL_VALUES=`grep -v '^#' "$1" | sed -E 's|^([^=]+)=(.*)$|\1="${\1:-\2}"|g'`
-    eval `echo ${_INITIAL_VALUES}`
+    eval "${_INITIAL_VALUES}"
 
     local _NAMES=`grep -v '^#' "$1" | sed -E 's|^([^=]+)=(.*)$|\1|g'`
     for VARNAME in ${_NAMES}
