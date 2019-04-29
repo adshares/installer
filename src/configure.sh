be530e5f9358db390e3056a30a85a86c52ecad56
Paweł Zaremba <pawzar@gmail.com>
2019-04-29 19:39:26 +0200

    test

--- src/configure.sh
+++ src/configure.sh
@@ -89,8 +89,9 @@ then
 
     read_env ${VENDOR_DIR}/adpanel/.env || read_env ${VENDOR_DIR}/adpanel/.env.dist
 echo -e "\n\n${VENDOR_DIR}/adpanel/.env"
+cat ${VENDOR_DIR}/adpanel/.env
 echo -e "\n\n####"
-echo -e "\n\n$APP_CURRENCY_CODE\t$APP_CURRENCY_SYMBOL\n\n"
+echo -e "\n\n${APP_CURRENCY_CODE}\t${APP_CURRENCY_SYMBOL}\n\n"
 
     ADPANEL_URL="${INSTALL_SCHEME}://$INSTALL_HOSTNAME"
 
