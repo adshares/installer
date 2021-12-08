09df3483241bf069f68c619a453b47322fbc4846
Christoph <christoph@rustige.me>
2021-12-08 09:06:35 +0100

    updated to php7.4

--- src/bootstrap.sh
+++ src/bootstrap.sh
@@ -82,7 +82,7 @@ fi
 apt-get --yes update
 apt-get --yes --no-install-recommends install \
     python python-pip python-dev gcc \
-    php7.2-fpm php7.2-mysql php7.2-bcmath php7.2-bz2 php7.2-curl php7.2-gd php7.2-intl php7.2-mbstring php7.2-sqlite3 php7.2-zip php7.2-simplexml php-apcu \
+    php7.4-fpm php7.4-mysql php7.4-bcmath php7.4-bz2 php7.4-curl php7.4-gd php7.4-intl php7.4-mbstring php7.4-sqlite3 php7.4-zip php7.4-simplexml php-apcu \
     ads nginx percona-server-server-5.7 percona-server-client-5.7 nodejs yarn \
     certbot python-certbot-nginx apt-transport-https \
     elasticsearch
